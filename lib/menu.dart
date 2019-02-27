928a4a05fdb8888647bfb96b93ce596fe845d4b5
pawelkalbarczyk <https://MrDeaE@bitbucket.org/MrDeaE/nowican.git>
2019-02-27 23:26:14 +0100

    menu

--- lib/menu.dart
+++ lib/menu.dart
@@ -3,26 +3,36 @@ import 'package:flutter/material.dart';
 import './hex_color.dart';
 import './main.dart';
 
-
-
 class MainMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('MainMenu created');
     return new Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        Row(
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          alignment: Alignment.center,
+          constraints: BoxConstraints.expand(height: 300, width: 400),
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/logo.jpg'))),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+          child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[build1PButton(context)],
-        )
+          children: <Widget>[
+            buildGameButton(context, '1 Player'),
+            buildGameButton(context, '2 Players')
+          ],
+        ),
+        ),
+        buildGameButton(context, 'Rules'),
       ],
     );
   }
 
-  Widget build1PButton(BuildContext context) {
+  Widget buildGameButton(BuildContext context, String inStr) {
     print('build 1p button created');
     return Container(
       margin: const EdgeInsets.all(15.0),
@@ -30,17 +40,32 @@ class MainMenu extends StatelessWidget {
       decoration: BoxDecoration(
           border: Border.all(color: HexColor('333333')),
           borderRadius: BorderRadius.all(Radius.circular(3.0))),
-      child: RawMaterialButton(
-          child: Text('Start 1p Game'),
-          onPressed:  () {
-            click1pGame(context);
-          }
-          ),
+      child: FlatButton(
+          child: Text(inStr),
+          onPressed: () {
+            switch (inStr) {
+              case "1 Player":
+                click1pGame(context);
+                break;
+              case '2 Players':
+                click2pGame(context);
+                break;
+              case 'Rules':
+                null;
+                break;
+              default:
+            }
+          }),
     );
   }
 
   void click1pGame(BuildContext context) {
-    print("button start 1p game"); //1
-    Navigator.of(context).pushNamed('/screen1pGame'); //2
+    print("button start 1p game");
+    Navigator.of(context).pushNamed('/screen1pGame');
+  }
+
+  void click2pGame(BuildContext context) {
+    print('button start 2p game');
+    // Navigator.of(context).pushNamed('/screen1pGame');
   }
 }
